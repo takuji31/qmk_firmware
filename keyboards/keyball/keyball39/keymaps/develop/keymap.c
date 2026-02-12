fac28581d25d667d87362450aca2410f7c3d89c2
Takuji Nishibayashi <takuji31@gmail.com>
2026-02-13 00:16:10 +0900

    feat: make keyball39 buildable with current QMK firmware
    - Replace info.json with keyboard.json including matrix mappings for all layouts
    - Update deprecated RGBLED_NUM to RGBLIGHT_LED_COUNT in config.h
    - Remove manual LAYOUT macros from keyball39.h (auto-generated from keyboard.json)
    - Fix BL constant in shared keyball.c to use #define for C static initializer compatibility
    - Update pointing_device_driver_init return type from void to bool
    - Replace deprecated keycodes: KC_MS_BTN -> MS_BTN, RGB_* -> UG_* (underglow)

--- keyboards/keyball/keyball39/keymaps/develop/keymap.c
+++ keyboards/keyball/keyball39/keymaps/develop/keymap.c
@@ -31,9 +31,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [1] = LAYOUT_universal(
-    RGB_TOG  , _______  , _______  , _______  , _______  ,                            RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , SCRL_DVI ,                            RGB_M_K  , RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , SCRL_DVD ,                            CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
+    UG_TOGG  , _______  , _______  , _______  , _______  ,                            RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN ,
+    UG_NEXT  , UG_HUEU  , UG_SATU  , UG_VALU  , SCRL_DVI ,                            RGB_M_K  , RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW ,
+    UG_PREV  , UG_HUED  , UG_SATD  , UG_VALD  , SCRL_DVD ,                            CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
     QK_BOOT  , KBC_RST  , _______  , _______  , _______  , _______  ,      _______  , _______  , _______  , _______  , KBC_RST  , QK_BOOT
   ),
 };
